4c1043f2b1d9a42f2b99bf62e1b3eae2d1fce638
gambhirjha38 <jhagambhir38@gmail.com>
2025-06-25 15:25:38 +0530

    Create Request.java

---
+++ Request.java
@@ -0,0 +1,18 @@
+// Request.java
+public class Request {
+    private int floor;
+    private Direction direction;
+
+    public Request(int floor, Direction direction) {
+        this.floor = floor;
+        this.direction = direction;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+}
